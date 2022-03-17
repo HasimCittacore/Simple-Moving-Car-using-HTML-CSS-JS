11a00a9862946071590f05198fb3c3a6179d74d7
HasimNewCoder <462mohammed@gmail.com>
2022-03-18 02:05:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+
+document.addEventListener('click', function () {
+    console.log('The document was clicked');
+ });
+ 
+ document.body.addEventListener('click', function () {
+     console.log('The document body was clicked');
+ });
